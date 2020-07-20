286110d403d42e3e8ced6f4edbd3410416a3c7d9
anubhavbhai007 <63663575+anubhavbhai007@users.noreply.github.com>
2020-07-20 12:25:30 +0530

    Add files via upload

---
+++ web develpment/footer.php
@@ -0,0 +1,18 @@
+
+<div class=" navbar-dark bg-dark ">
+<div class="row ">
+<div class="col-4" style="text-align:center;color:white;">
+<h6>INFORMATION</h6>
+<span>About us</span><br>
+<span>Contactus</span>
+</div>
+<div class="col-4" style="text-align:center;color:white;">
+<h6>MY ACCOUNT</h6>
+<span>Settings</span>
+</div>
+<div class="col-4" style="text-align:center;color:white;">
+<h6>CONTACT</h6>
+<span>+91 89877636623</span>
+</div>
+</div>
+  </div>
